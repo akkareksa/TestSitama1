0d6fdc88bc1112b99ea6f6b5ed9b21ccb427b26f
akkareksa <vckevin98@gmail.com>
2023-02-14 14:29:58 +0700

    menambahkan dokumentasi method

--- src/test1/Test1.java
+++ src/test1/Test1.java
@@ -7,7 +7,7 @@ package test1;
 import java.util.Scanner;
 
 /**
- *
+ * test backend sitama no 1 
  * @author vckev
  */
 public class Test1 {
@@ -22,6 +22,11 @@ public class Test1 {
         System.out.println(loopingMultiplier10(input));
     }
     
+    /**
+     * Melakukan perkalian angka dari angka 1 hingga 10
+     * @param n Integer : angka integer yang ingin dilakukan perkalian dari angka 1 hingga 10
+     * @return result String : penjabaran dari perkalian angka yang dilakukan operasi perkalian
+     */
     public static String loopingMultiplier10(int n) {
         String res = "";
         for(int i = 1; i <= 10; i++) {
